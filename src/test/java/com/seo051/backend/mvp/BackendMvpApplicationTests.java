6fbcc0ddfff2da6eb069f5283b8a8fc85cd2b460
Seo-051 <kingdom5437@naver.com>
2026-01-23 14:39:18 +0900

    chore: initialize spring boot project

---
+++ src/test/java/com/seo051/backend/mvp/BackendMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seo051.backend.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
